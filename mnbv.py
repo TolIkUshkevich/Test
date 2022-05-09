778e0fd7976723896e048cfdf9e0ed67d801b200
TolIk <anatoliyyushkevich@gmail.com>
2022-05-09 12:03:38 +0300

    привет, задача с codewars  не получается. задача не понятная, + в pycharm все работает, а там ошибка

--- mnbv.py
+++ mnbv.py
@@ -1,15 +1,19 @@
 def function(text):
-    if text[0] != text[0].upper():
-        text = text.replace('-', ' ')
-        text = text.replace('_', ' ')
-        text = text.title()
-        text = text.replace(' ', '')
-        text = text.replace(text[0], text[0].lower())
-    else:
-        text = text.replace('-', ' ')
-        text = text.replace('_', ' ')
-        text = text.title()
-        text = text.replace(' ', '')
+    try:
+        if text[0] != text[0].upper():
+            text = text.replace('-', ' ')
+            text = text.replace('_', ' ')
+            text = text.title()
+            text = text.replace(' ', '')
+            text = text.replace(text[0], text[0].lower())
+        else:
+            text = text.replace('-', ' ')
+            text = text.replace('_', ' ')
+            text = text.title()
+            text = text.replace(' ', '')
+        return text
+    except IndexError:
+        return ''
 
-    return text
+
 print(function(input()))
